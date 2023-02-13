358b3ecc1880d74725f92f95d2b1d16b46e1240c
asawadogo98 <124659269+asawadogo98@users.noreply.github.com>
2023-02-13 17:56:51 -0500

    Update 2023-02-13-Black-History-Month.d

--- _posts/2023-02-13-Black-History-Month.d
+++ _posts/2023-02-13-Black-History-Month.d
@@ -7,10 +7,10 @@ gh-badge: [star, fork, follow]
 tags: [test]
 comments: true
 ---
-To celebrate my Black life, I'm going to list 5 reasons why
+**To celebrate my Black life, I'm going to list 5 reasons why**
 ![alt text](https://github.com/asawadogo98/asawadogo98.github.io/assests/img/77351.jpg)
-1. Beacause my mother, who will never let me forget that she born me. 
-1. Black youtube naturaturals who convinced me I should wait at least 3-4 years to see theresults of their potions. 
-1. Mac and Cheese
-1. Twitter during the pandemic 
-1. Poorly executing TikTok dances
+1.Beacause my mother, who will never let me forget that she born me. 
+1.Black youtube naturaturals who convinced me I should wait at least 3-4 years to see theresults of their potions. 
+1.Mac and Cheese
+1.Twitter during the pandemic 
+1.Poorly executing TikTok dances
