bf78b30e6d4f86c22609c4214cadcf9351351cc7
asawadogo98 <124659269+asawadogo98@users.noreply.github.com>
2023-02-13 20:37:34 -0500

    Update 2023-02-13-Black-History-Month.d

--- _posts/2023-02-13-Black-History-Month.d
+++ _posts/2023-02-13-Black-History-Month.d
@@ -10,7 +10,7 @@ comments: true
 #To celebrate my Black life, I'm going to list 5 reasons why:
 ! [This is alt text] (https://asawadogo98.github.io/assets/img/unnamed.jpg)
 1. Because my mother, who will never let me forget that she born me. 
-1. Black youtube naturals who convinced me I should wait at least 3-4 years to see the results of their potions. 
+1. Black youtube naturals who convinced me I should wait at least three or four years to see the results of their potions. 
 1. Mac and Cheese
 1. Twitter during the pandemic 
 1. Poorly executing TikTok dances
